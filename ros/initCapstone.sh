2dd208cc4911548e4539c33018a47644a03336b1
Antonia Reiter <antonia.reiter@freenet.de>
2017-11-12 15:47:30 +0100

    logging added correctly

--- ros/initCapstone.sh
+++ ros/initCapstone.sh
@@ -2,3 +2,7 @@
 cd src
 rm CMakeLists.txt
 catkin_init_workspace
+if [ ! -d "${logdirectory}" ]
+then
+    mkdir -p "${logdirectory}"
+fi
--- ros/runme.sh
+++ ros/runme.sh
@@ -2,7 +2,8 @@
 
 # exit on first error
 set -e
-
+logdirectory="/capstone/log"
+export ROS_LOG_DIR=${logdirectory}
 catkin_make
 source devel/setup.bash
 roslaunch launch/styx.launch
