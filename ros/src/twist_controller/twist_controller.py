6e4e8c54aafe8beb4cf1917a17e7a2f83b55c2e9
Peter Barry <pete.barry@gmail.com>
2017-11-18 18:52:33 +0000

    Added standard Yaw Controller.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,5 +1,5 @@
 import rospy
-
+from yaw_controller import YawController
 
 
 GAS_DENSITY = 2.858
@@ -7,22 +7,30 @@ ONE_MPH = 0.44704
 
 
 class TwistController(object):
-    def __init__(self, **kwargs):
+    def __init__(self, **veh_caps):
 
-        # TODO: Implement
+        # Capture key vehicle capabilites
 
-        rospy.logwarn(str(kwargs))
-        self.vehicle_mass =     kwargs['vehicle_mass']
-        self.fuel_capacity =    kwargs['fuel_capacity']
-        self.brake_deadband =   kwargs['brake_deadband']
-        self.decel_limit =      kwargs['decel_limit']
-        self.accel_limit =      kwargs['accel_limit']
-        self.wheel_radius =     kwargs['wheel_radius']
-        self.wheel_base =       kwargs['wheel_base']
-        self.steer_ratio =      kwargs['steer_ratio']
-        self.max_lat_accel =    kwargs['max_lat_accel']
-        self.max_steer_angle =  kwargs['max_steer_angle']
+        rospy.logwarn(str(veh_caps))
+        self.vehicle_mass =     veh_caps['vehicle_mass']
+        self.fuel_capacity =    veh_caps['fuel_capacity']
+        self.brake_deadband =   veh_caps['brake_deadband']
+        self.decel_limit =      veh_caps['decel_limit']
+        self.accel_limit =      veh_caps['accel_limit']
+        self.wheel_radius =     veh_caps['wheel_radius']
+        self.wheel_base =       veh_caps['wheel_base']
+        self.steer_ratio =      veh_caps['steer_ratio']
+        self.max_lat_accel =    veh_caps['max_lat_accel']
+        self.max_steer_angle =  veh_caps['max_steer_angle']
 
+        self.min_speed = 0.0
+
+        # 1st implementation of steering is to use provided Yaw Controller.
+        self.yaw = YawController( self.wheel_base,
+                                  self.steer_ratio,
+                                  self.min_speed,
+                                  self.max_lat_accel,
+                                  self.max_steer_angle)
 
 
     def control(self,
@@ -31,6 +39,14 @@ class TwistController(object):
                 current_linear_vel,
                 dbw_enabled):
 
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+        throttle = 1.0
+        brake = 0.0
+        steer = 0.0
+
+
+        # TODO: For Steerig - use provided yaw controller.
+
+        if (dbw_enabled):
+            steer = self.yaw.get_steering( proposed_linear_vel, proposed_angular_vel, current_linear_vel)
+
+        return throttle,brake,steer
